64d3a080e813f25bb3845350e2ef881db5a431ba
Seth Hernandez <sethmichaelhernandez@gmail.com>
2026-07-06 17:55:19 +0800

    feat(context): Add the notification and validation on cart context
    - Add clear cart.
    - Add remove product from cart
    - Add mockup data in storeData.tsx.

--- cart-engine/src/main.tsx
+++ cart-engine/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { NotificationContextProvider } from './context/Notification.tsx'
+import { CartContextProvider } from './context/CartContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NotificationContextProvider>
-      <App />
-    </NotificationContextProvider>
+    <CartContextProvider>
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
+    </CartContextProvider>
   </StrictMode>,
 )
