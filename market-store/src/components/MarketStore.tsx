489f7b20857c7c77b9f16e06dc657b4a66758821
Seth Hernandez <sethmichaelhernandez@gmail.com>
2026-07-15 17:11:57 +0800

    feat(ui): Finish category ui with working filtering func
    - Finish category filtering and successfully extracting the unique category and loop it while adding the "all" value.

--- market-store/src/components/MarketStore.tsx
+++ market-store/src/components/MarketStore.tsx
@@ -8,22 +8,26 @@ type Product = {
   image: string
 }
 
-
 const MarketStore = () => {
   const [product, setProduct] = useState<Product[]>([]);
+  const [category, setCategory] = useState<string[]>([]);
+  const [activeCategory, setActiveCategory] = useState("all");
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
   const fetchProducts = async () => {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
-
       if(!response.ok) {
         throw new Error(`HTTP Error Status: ${response.status}`)
       }
       const data = await response.json();
+      const categoryOnly = data.map((val) => val.category)
+      const noDuplicate = [...new Set(categoryOnly)]
+      const allCat = ["all", ...noDuplicate]
       setProduct(data)
-      
+      setCategory(allCat)
+
     } catch(error) {
       setError('Failed to load products. Please try again.')
     } finally {
@@ -34,6 +38,10 @@ const MarketStore = () => {
   useEffect(() => {
     fetchProducts();
   }, [])
+
+  const handleCategoryClick = (cat: string) => {
+    setActiveCategory(cat)
+  }
 
   if(isLoading) {
     return (
@@ -46,26 +54,34 @@ const MarketStore = () => {
     )
   }
 
-
-  console.log(product)
   return (
     <div className="min-h-screen p-6 bg-gray-50"> 
       <section className="flex flex-col gap-8 md:flex-row">
-        <aside className="w-full rounded-lg shadow-md md:w-1/4">
-          <h2 className="text-2xl font-bold text-center text-blue-500">Categories</h2>
+        <aside className="sticky w-full p-6 mt-20 border rounded-lg shadow-md h-155 min-h-155 top-6 md:w-1/4">
+          <h2 className="m-6 text-2xl font-bold text-center text-blue-500">Categories</h2>
+
+          {category && category.map((val, index) => (
+            <button 
+              key={index} 
+              onClick={() => handleCategoryClick(val)}
+              className={`capitalize underline block cursor-pointer underline-offset-2 ${activeCategory === val && "text-blue-500"}`}
+            >
+              {val}
+            </button>
+          ))}
         </aside>
         
         <article className="w-full md:w-3/4">
-          <h2 className="text-2xl font-bold text-center text-blue-500">Market Store</h2>
+          <h2 className="m-6 text-2xl font-bold text-center text-blue-500">Market Store</h2>
 
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {product.map((prod) => {
+            {product.filter((cat) => activeCategory === "all" ? true : cat.category === activeCategory).map((prod) => {
               return (
                 <div className="flex flex-col h-full p-4 border rounded-lg" key={prod.id}>
                   <div className="h-32 mb-4">
                     <img src={prod.image} className="object-contain w-full h-full" alt={prod.title} /> 
                   </div>
-                  <div className="flex flex-col">
+                  <div className="flex flex-col grow">
                     <h3 className="text-lg font-semibold text-gray-900 capitalize md:text-lg line-clamp-2">{prod.title}</h3>
                     <div className="mt-auto">
                       <p className="text-green-700">₱ {prod.price}</p>
