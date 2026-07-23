f193756567c50928f2c4182d4f1067172f8b6cab
Seth Hernandez <sethmichaelhernandez@gmail.com>
2026-07-23 18:03:59 +0800

    feat(ui): Add the add to cart functionality

--- market-store/src/components/MarketStore.tsx
+++ market-store/src/components/MarketStore.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useState, type ChangeEvent } from "react"
 import Sidebar from "./Sidebar"
 import SearchBar from "./SearchBar"
-
-type Product = {
-  id: number
-  title: string
-  price: number
-  category: string
-  image: string
-}
+import type { CartItem, Product } from "../data/Data"
+import { useCart } from "../context/CartContext"
+import CustomButton from "./CustomButton"
 
 const MarketStore = () => {
   const [product, setProduct] = useState<Product[]>([]);
@@ -18,6 +13,12 @@ const MarketStore = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
+  const {cart, addToCart, removeFromCart, clearCart} = useCart();
+
+  useEffect(() => {
+    console.log("Cart updated! Current items:", cart);
+  }, [cart]);
+
   const fetchProducts = async () => {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
@@ -25,12 +26,15 @@ const MarketStore = () => {
         throw new Error(`HTTP Error Status: ${response.status}`)
       }
       const data = await response.json();
+      const addFakeStocks = data.map((prod) => ({
+        ...prod,
+        stocks: 10
+      }))
       const categoryOnly = data.map((val) => val.category)
       const noDuplicate = [...new Set(categoryOnly)]
       const allCat = ["all", ...noDuplicate]
-      setProduct(data)
+      setProduct(addFakeStocks)
       setCategory(allCat)
-
     } catch(error) {
       setError('Failed to load products. Please try again.')
     } finally {
@@ -48,7 +52,6 @@ const MarketStore = () => {
 
   const handleSearchQuery = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
-    console.log(e.target.value)
   }
 
   const tryAgain = () => {
@@ -78,7 +81,7 @@ const MarketStore = () => {
   }
 
   return (
-    <div className="min-h-screen p-6 bg-gray-50"> 
+    <div className="min-h-screen p-6 bg-gray-50 font-sans"> 
       <section className="flex flex-col gap-8 md:flex-row">
         <Sidebar 
           category={category} 
@@ -102,6 +105,10 @@ const MarketStore = () => {
                 return false
               }
             }).map((prod) => {
+              const itemInCart = cart.find((item) => item.id === prod.id)
+              const currentQuantity = itemInCart ? itemInCart.quantity : 0
+              const remainingStock = prod.stocks - currentQuantity
+
               return (
                 <div className="flex flex-col h-full p-4 border rounded-lg" key={prod.id}>
                   <div className="h-32 mb-4">
@@ -112,6 +119,14 @@ const MarketStore = () => {
                     <div className="mt-auto">
                       <p className="text-green-700">₱ {prod.price}</p>
                       <p className="text-blue-500 capitalize">{prod.category}</p>
+                      <p>Stocks: {remainingStock}</p>
+                      <CustomButton 
+                        className={`border py-1 px-2 mt-2 rounded-sm active:scale-95 duration-300 ease-in-out ${remainingStock ? 'cursor-pointer bg-green-700 text-white' : 'bg-gray-300 cursor-not-allowed'}`}
+                        onClick={() => addToCart(prod)}
+                        disabled={remainingStock === 0}
+                      >
+                        Add to cart
+                      </CustomButton>
                     </div>
                   </div>
                 </div>
