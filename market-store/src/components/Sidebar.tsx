869cdd96edb2237ff215b9279d3f5e71e2228562
Seth Hernandez <sethmichaelhernandez@gmail.com>
2026-07-30 07:25:39 +0800

    refactor(ui): Update the sidebar design

--- market-store/src/components/Sidebar.tsx
+++ market-store/src/components/Sidebar.tsx
@@ -6,14 +6,14 @@ type SidebarType = {
 
 const Sidebar = ({category, handleCategoryClick, activeCategory}: SidebarType) => {
   return (
-    <aside className="sticky w-full p-6 mt-20 border rounded-lg shadow-md h-155 min-h-155 top-6 md:w-1/4">
+    <aside className="w-full p-6 mt-20 border rounded-lg shadow-md lg:sticky h-155 min-h-155 top-10 md:w-1/4">
       <h2 className="m-6 text-2xl font-bold text-center text-gray-800">Categories</h2>
 
       {category && category.map((val, index) => (
         <button 
           key={index} 
           onClick={() => handleCategoryClick(val)}
-          className={`capitalize underline block cursor-pointer underline-offset-2 ${activeCategory === val && "text-blue-500"}`}
+          className={`capitalize block cursor-pointer ease-in-out ${activeCategory === val && "text-blue-500 scale-110"}`}
         >
           {val}
         </button>
